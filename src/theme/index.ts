80c5ab21a0906587a51d40fb10720db288701ae0
Henrique Nunes <henri.arantes1@gmail.com>
2023-05-28 20:50:23 -0300

    add app base config

---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+
+    GRAY_7: '#1B1D1E',
+    GRAY_6: '#333638',
+    GRAY_5: '#5C6265',
+    GRAY_4: '#B9BBBC',
+    GRAY_3: '#DDDEDF',
+    GRAY_2: '#EFF0F0',
+    GRAY_1: '#FAFAFA',
+    WHITE: '#FFFFFF',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold',
+  },
+  FONT_SIZE: {
+    XXS: 12,
+    XS: 14,
+    SM: 16,
+    MD: 18,
+    LG: 24,
+    XL: 32,
+  },
+}
